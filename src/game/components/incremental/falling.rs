3c339dd0f8993dbe6209f99f29f935a230eafc25
Niels Saurer <me@nielssaurer.com>
2025-02-04 22:08:00 +0100

    make fixed tick much simpler

--- src/game/components/incremental/falling.rs
+++ src/game/components/incremental/falling.rs
@@ -14,8 +14,7 @@ impl FallingSimulationData {
 }
 
 pub struct FallingSimulationComponent {
-    next_update: Instant,
-    last_update: Instant,
+    dt_budget: f64,
 }
 
 impl FallingSimulationComponent {
@@ -24,8 +23,7 @@ impl FallingSimulationComponent {
 
     pub fn new() -> Self {
         Self {
-            next_update: Instant::now(),
-            last_update: Instant::now(),
+            dt_budget: 0.0,
         }
     }
 
@@ -41,24 +39,14 @@ impl Component for FallingSimulationComponent {
     }
 
     fn update(&mut self, update_info: UpdateInfo, shared_state: &mut SharedState) {
-        if Instant::now() < self.next_update {
-            return;
-        }
+        let dt = (update_info.current_time - update_info.last_time).as_secs_f64();
+        self.dt_budget += dt;
+        
 
-        // note: use 'self.last_update' as dt computation, because that is the delta since we last
-        // processed.
-        let mut remaining_dt = (update_info.current_time - self.last_update).as_secs_f64();
-        // dt is the amount of time that has passed, and is our budget for updating the simulation
-        // in case the fps is locked to eg 30, then our budget is doubled.
-
-        while remaining_dt >= Self::UPDATE_INTERVAL {
+        while self.dt_budget >= Self::UPDATE_INTERVAL {
             self.update_simulation(shared_state);
-            remaining_dt -= Self::UPDATE_INTERVAL;
-            // use the leftover remaining_dt (assuming we're in last iteration) to move last_update a bit farther back and give us more time
-            self.last_update = Instant::now() - Duration::from_secs_f64(remaining_dt);
+            self.dt_budget -= Self::UPDATE_INTERVAL;
         }
-
-        self.next_update += Duration::from_secs_f64(Self::UPDATE_INTERVAL);
     }
 
     fn render(&self, mut renderer: &mut dyn Renderer, shared_state: &SharedState, depth_base: i32) {
