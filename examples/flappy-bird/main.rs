79a4e4b746437c7b6b7b660a9abcb35999f17ae9
Niels Saurer <me@nielssaurer.com>
2025-02-16 23:07:29 +0100

    init flappy bird example

---
+++ examples/flappy-bird/main.rs
@@ -0,0 +1,12 @@
+use std::io;
+use std::io::stdout;
+use teng::components::KeyPressRecorderComponent;
+use teng::Game;
+
+fn main() -> io::Result<()> {
+    let mut game = Game::new(stdout());
+    game.add_component(Box::new(KeyPressRecorderComponent::new()));
+    
+    
+    game.run()
+}
